cabd936376695413c8191ca5c63262ae93cb92eb
J-non <whdtjr1513@naver.com>
2024-09-26 21:28:00 +0900

    [feat]: React Native 네비게이트 음식앱(미완)

---
+++ 3_practice/screens/CategoriesScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CATEGORIES } from '../data/dummy-data'
+import { FlatList } from 'react-native'
+
+
+const CategoriesScreen = () => {
+  return (
+    <>
+      <FlatList />
+    </>
+  )
+}
+
+export default CategoriesScreen
